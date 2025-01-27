02372554a43640e0b63da1448d16d63311c3e832
Jeremiah <jeremiahjacob261@gmail.com>
2025-01-27 18:08:39 +0100

    wrong ext

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta property="og:description" content="A fun and engaging 2D car game by Jerry. Play now and enjoy the excitement!"/>
       <meta property="og:type" content="website"/>
       <meta property="og:url" content="https://2dgame-six.vercel.app"/>
-      <meta property="og:image" content="https://2dgame-six.vercel.app/car.jpg"/>
+      <meta property="og:image" content="https://2dgame-six.vercel.app/car.png"/>
       <meta name="twitter:card" content="summary_large_image"/>
       <meta name="twitter:title" content="2D Car Game by Jerry"/>
       <meta name="twitter:description" content="A fun and engaging 2D car game by Jerry. Play now and enjoy the excitement!"/>
-      <meta name="twitter:image" content="https://2dgame-six.vercel.app/car.jpg"/>
+      <meta name="twitter:image" content="https://2dgame-six.vercel.app/car.png"/>
       </head>
       <body className="bg-gray-100">{children}</body>
     </html>
